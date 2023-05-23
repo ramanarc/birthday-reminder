7404de4660010e579b8342d8391b0b2bf8349370
Anuj Raman <anujramanc@gmail.com>
2023-05-23 07:55:01 +0530

    create card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+
+const Card = () => {
+  return (
+    <div className='card-wrap'>
+      <img src="https://www.course-api.com/images/people/person-1.jpeg" alt="" />
+      <div className='card-content'>
+        <h2>Name</h2>
+        <h4>Age</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,18 +1,11 @@
+import Card from "./Card"
 
 
 const CardContainer = () => {
   return (
     <div className='card-container'>
-      {/* Card Title Dynamic */}
       <h1>5 Birthdays Today</h1>
-      {/* Card */}
-      <div className='card-wrap'>
-        <img src="https://www.course-api.com/images/people/person-1.jpeg" alt="" />
-        <div className='card-content'>
-          <h2>Name</h2>
-          <h4>Age</h4>
-        </div>
-      </div>
+      <Card />
       <button>Clear All</button>
     </div>
   )
