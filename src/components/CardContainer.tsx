8e6890504e8b2d40291ba7e02cbbd04cef962ee0
Anuj Raman <anujramanc@gmail.com>
2023-05-23 07:53:42 +0530

    add css and structure

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,14 +1,17 @@
 
+
 const CardContainer = () => {
   return (
     <div className='card-container'>
       {/* Card Title Dynamic */}
       <h1>5 Birthdays Today</h1>
       {/* Card */}
-      <div>
-        <img src="" alt="" />
-        <h4>Name</h4>
-        <p>Age</p>
+      <div className='card-wrap'>
+        <img src="https://www.course-api.com/images/people/person-1.jpeg" alt="" />
+        <div className='card-content'>
+          <h2>Name</h2>
+          <h4>Age</h4>
+        </div>
       </div>
       <button>Clear All</button>
     </div>
